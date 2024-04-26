8c565683a865bdf6e735fe2bf7ec9cad2484c581
Chetan Singh <122537861+xchetan9@users.noreply.github.com>
2024-04-26 04:59:07 +0000

    version 1

---
+++ script.js
@@ -0,0 +1,161 @@
+!function () {
+    "use strict";
+    function e(e) {
+        localStorage.measure = e.target.id,
+            t.localStorage = localStorage,
+            document.getElementById("ms").classList.remove("active"),
+            document.getElementById("mh").classList.remove("active"),
+            document.getElementById("kh").classList.remove("active"),
+            e.target.classList.add("active")
+    }
+    document.addEventListener("DOMContentLoaded", function (e) {
+        if (t.init(),
+            localStorage && !localStorage.getItem("userClosedHelpModal"))
+            setTimeout(function () { }, 5e3);
+        else if (localStorage && localStorage.getItem("userClosedHelpModal")) {
+            var n = localStorage.getItem("userClosedHelpModal")
+                , o = new Date;
+            (o.getTime() - n) / 864e5 > 20 && localStorage.removeItem("userClosedHelpModal")
+        }
+    });
+    var t = {};
+    t.cache = {},
+        t.opts = {
+            maxSpeed: 200
+        },
+        t.utils = {},
+        t.localStorage = localStorage,
+        t.showHelp = function () {
+            document.getElementById("modal").style.display = "block"
+        }
+        ,
+        t.initBtnClickListners = function () {
+            document.getElementById("ms").addEventListener("click", e),
+                document.getElementById("mh").addEventListener("click", e),
+                document.getElementById("kh").addEventListener("click", e)
+        }
+        ,
+        t.init = function () {
+            t.cache.speed = document.getElementById("speed"),
+                t.localStorage.measure || (localStorage.measure = "kh",
+                    t.localStorage = localStorage),
+                t.initBtnClickListners(),
+                document.getElementById("installer").style.display = "none",
+                t.writeSpeed = function (e) {
+                    document.getElementById("error").innerHTML = "",
+                        document.getElementById("unit").innerHTML = "meter/second";
+                    var n = e.coords.speed;
+                    "kh" === t.localStorage.measure && (n = 60 * n * 60,
+                        n /= 1e3,
+                        document.getElementById("unit").innerHTML = "km/h"),
+                        "mh" === t.localStorage.measure && (n = 60 * n * 60,
+                            n = .000621371192 * n,
+                            document.getElementById("unit").innerHTML = "miles/hour"),
+                        n = Math.round(n),
+                        t.cache.speed.innerHTML = n
+                }
+                ,
+                navigator.geolocation.watchPosition(function (e) {
+                    t.writeSpeed(e);
+                    try {
+                        ga && ga("send", {
+                            hitType: "event",
+                            eventCategory: "speed",
+                            eventAction: "showSpeed",
+                            eventLabel: e.coords.speed
+                        })
+                    } catch (n) { }
+                    document.getElementById("lat").innerHTML = "",
+                        document.getElementById("long").innerHTML = "",
+                        document.getElementById("info").style.display = "none"
+                }, function (e) {
+                    document.getElementById("speed").innerHTML = "",
+                        document.getElementById("error").innerHTML = "ERROR(" + e.code + "): " + e.message,
+                        document.getElementById("lat").innerHTML = "ERROR!!!",
+                        document.getElementById("info").style.display = "block";
+                    try {
+                        ga && ga("send", {
+                            hitType: "event",
+                            eventCategory: "speed",
+                            eventAction: "errorShowSpeed",
+                            eventLabel: e.message
+                        })
+                    } catch (t) { }
+                }, {
+                    enableHighAccuracy: !0,
+                    maximumAge: 0
+                })
+        }
+        ;
+    var n = function (e) {
+        var t = (e.querySelector(".tooltip"),
+            function (t) {
+                t.preventDefault(),
+                    window.install.prompt().then(function (t) {
+                        try {
+                            ga && ga("send", "event", "install", t)
+                        } catch (n) { }
+                        e.classList.remove("available")
+                    })["catch"](function (e) {
+                        try {
+                            ga && ga("send", "event", "install", "errored")
+                        } catch (t) { }
+                    })
+            }
+        )
+            , n = function () {
+                window.install.canPrompt().then(function () {
+                    document.getElementById("installer").style.display = "block",
+                        e.classList.add("available");
+                    try {
+                        ga && ga("send", "event", "install", "prompted")
+                    } catch (t) { }
+                })
+            };
+        e.addEventListener("click", t.bind(this)),
+            e.addEventListener("touchend", t.bind(this)),
+            n()
+    };
+    !function () {
+        var e, t, n = !1, o = new Promise(function (e, n) {
+            t = e
+        }
+        );
+        window.addEventListener("beforeinstallprompt", function (o) {
+            return n = !0,
+                o.preventDefault(),
+                e = o,
+                t(),
+                !1
+        });
+        var a = {};
+        Object.defineProperty(a, "isAvailable", {
+            get: function () {
+                return n
+            }
+        }),
+            a.canPrompt = function () {
+                return o
+            }
+            ,
+            a.prompt = function () {
+                return new Promise(function (t, o) {
+                    n === !1 && o("User Agent decided not to prompt"),
+                        e.prompt().then(function () {
+                            return e.userChoice
+                        }).then(function (e) {
+                            t(e.outcome)
+                        })["catch"](function (e) {
+                            o(e)
+                        })
+                }
+                )
+            }
+            ,
+            window.install = a
+    }(),
+        window.addEventListener("load", function () {
+            var e = document.getElementById("installer");
+            new n(e)
+        })
+}();
---
+++ service-worker.js
@@ -0,0 +1,82 @@
+var CACHE_NAME = 'zpssseed';
+var FilesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FilesToCache)
+      .then(() => self.skipWaiting());
+    })
+    .then(() => {
+      // force waiting worker to become active worker (claim)
+      self.skipWaiting();
+    })
+    .catch(error => {
+      console.error('Pre-fetching failed:', error);
+    })
+  )
+});
+
+// self.addEventListener('activate',  event => {
+//   event.waitUntil(self.clients.claim());
+// });
+// Activating service worker
+self.addEventListener('activate', function(e) {
+  console.log('[Service Worker] Activate');
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        // Removing old cache
+        if (key !== CACHE_NAME) {
+          console.log('[Service Worker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+    .then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+  event.waitUntil(
+    update(event.request)
+      .then(refresh)
+  );
+});
+
+// TO Update Request
+function update(request) {
+  return caches.open(CACHE_NAME).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response.clone()).then(function () {
+        return response;
+      });
+    });
+  });
+}
+
+// TO Refresh Response
+function refresh(response) {
+  return self.clients.matchAll().then(function (clients) {
+    clients.forEach(function (client) {
+      var message = {
+        type: 'refresh',
+        url: response.url,
+        eTag: response.headers.get('ETag')
+      };
+    client.postMessage(JSON.stringify(message));
+    });
+  });
+}
